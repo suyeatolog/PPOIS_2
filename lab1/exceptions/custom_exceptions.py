15354ea63a6938937a1b783d8b07ed0c16bcfbb9
suyeatolog <pk080407@gmail.com>
2026-02-11 22:25:16 +0300

    feat: added custom exceptions for Sun class

--- lab1/exceptions/custom_exceptions.py
+++ lab1/exceptions/custom_exceptions.py
@@ -9,3 +9,9 @@ class InvalidRadiusError(SolarSystemError): #если радиус <= 0
 
 class InvalidPositionError(SolarSystemError): #если не 3 элемента передано в tuple для координат
     pass
+
+class InvalidTemperatureError(SolarSystemError): #если температура Солнца <= 0
+    pass
+
+class InvalidLuminosityError(SolarSystemError): #если светимость Солнца <= 0
+    pass
