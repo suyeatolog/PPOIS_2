e9b42f5823ed44811a0e4c5be1eec35838c0d44f
suyeatolog <pk080407@gmail.com>
2026-02-12 19:06:01 +0300

    feat: Asteroid_validator for validating asteroid class fields

---
+++ lab1/src/validators/asteroid_validator.py
@@ -0,0 +1,14 @@
+from exceptions.custom_exceptions import InvalidCompositionError, InvalidOrbitTypeError
+
+class AsteroidValidator:
+    @staticmethod
+    def validate_composition(composition: str):
+        valid_compositions = {"rocky", "metallic", "carbonaceous"}
+        if composition not in valid_compositions:
+            raise InvalidCompositionError(f"Недопустимый состав: {composition}")
+
+    @staticmethod
+    def validate_orbit_type(orbit_type: str):
+        valid_orbits = {"main belt", "near-earth", "trojan", "centaur", "trans-Neptunian"}
+        if orbit_type not in valid_orbits:
+            raise InvalidOrbitTypeError(f"Недопустимый тип орбиты: {orbit_type}")
