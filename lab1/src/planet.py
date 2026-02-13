a191906c5be879fd9e806f9df3ff276d123efbed
suyeatolog <pk080407@gmail.com>
2026-02-13 09:46:14 +0300

    fix: Now can validate planet surface

--- lab1/src/planet.py
+++ lab1/src/planet.py
@@ -1,14 +1,18 @@
 from src.celestial_body import CelestialBody
 from typing import Optional
-
+from src.validators.planet_validator import PlanetValidator
 
 class Planet(CelestialBody):
     def __init__(
         self,
         atmosphere: Optional[str] = None,
+        surface: str = "Rocky",
         has_rings: bool = False,
     ):
         super().__init__()
+        PlanetValidator.validate_surface(surface)
+
+        self._surface = surface
         self._atmosphere = atmosphere
         self._has_rings = has_rings
 
@@ -19,6 +23,15 @@ class Planet(CelestialBody):
     @atmosphere.setter
     def atmosphere(self, value: Optional[str]):
         self._atmosphere = value
+
+    @property
+    def surface(self) -> str:
+        return self._surface
+    
+    @surface.setter
+    def surface(self, value: str):
+        PlanetValidator.validate_surface(value)
+        self._surface = value
 
     @property
     def has_rings(self) -> bool:
@@ -34,4 +47,4 @@ class Planet(CelestialBody):
         self.position = (x + vx * dt, y, z)
 
     def get_surface_info(self) -> str:
-        return f"Поверхность планеты {self.name} изучена"
+        return f"Поверхность планеты {self.name}={self.surface}, атмосфера={self.atmosphere}"
