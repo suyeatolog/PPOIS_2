64cd43c2af82be8d98330dabf6aa557c24dd0ae3
suyeatolog <pk080407@gmail.com>
2026-02-13 09:06:21 +0300

    feat: Custom exceptions for satellite validator

--- lab1/exceptions/custom_exceptions.py
+++ lab1/exceptions/custom_exceptions.py
@@ -1,35 +1,44 @@
-class SolarSystemError(Exception): #базовое исключение для солнечной системы
+class SolarSystemError(Exception): # базовое исключение для солнечной системы
     pass
 
-class InvalidMassError(SolarSystemError): #если масса <= 0
+class InvalidMassError(SolarSystemError): # если масса <= 0
     pass
 
-class InvalidRadiusError(SolarSystemError): #если радиус <= 0
+class InvalidRadiusError(SolarSystemError): # если радиус <= 0
     pass
 
-class InvalidPositionError(SolarSystemError): #если не 3 элемента передано в tuple для координат
+class InvalidPositionError(SolarSystemError): # если не 3 элемента передано в tuple для координат
     pass
 
-class InvalidTemperatureError(SolarSystemError): #если температура Солнца <= 0
+class InvalidTemperatureError(SolarSystemError): # если температура Солнца <= 0
     pass
 
-class InvalidLuminosityError(SolarSystemError): #если светимость Солнца <= 0
+class InvalidLuminosityError(SolarSystemError): # если светимость Солнца <= 0
     pass
 
-class InvalidCoreDiameterError(SolarSystemError): #если радиус ядра кометы <= 0
+class InvalidCoreDiameterError(SolarSystemError): # если радиус ядра кометы <= 0
     pass
 
-class InvalidPeriodError(SolarSystemError): #если период кометы <= 0
+class InvalidPeriodError(SolarSystemError): # если период кометы <= 0
     pass
 
-class InvalidEccentricityError(SolarSystemError): #если эксцентреситет орбиты кометы < 0 или > 1
+class InvalidEccentricityError(SolarSystemError): # если эксцентреситет орбиты кометы < 0 или > 1
     pass
 
-class InvalidTailLengthError(SolarSystemError): #если длина хвоста кометы < 0
+class InvalidTailLengthError(SolarSystemError): # если длина хвоста кометы < 0
     pass
 
-class InvalidCompositionError(SolarSystemError): #если состав не каменный/углеродный(карбонат)/металлический
+class InvalidCompositionError(SolarSystemError): # если состав не каменный/углеродный(карбонат)/металлический
     pass
 
-class InvalidOrbitTypeError(SolarSystemError): #если орбита астероида не: Главного пояса, Околоземная, Троянская, Кентавр, Транснептуновая
+class InvalidOrbitTypeError(SolarSystemError): # если орбита астероида не: Главного пояса, Околоземная, Троянская, Кентавр, Транснептуновая
+    pass
+
+class InvalidOrbitedPlanetError(SolarSystemError): # если планеты нет в Солнечной системе, или пустая строка
+    pass
+
+class InvalidOrbitalPeriodError(SolarSystemError): # если период спутника <= 0
+    pass
+
+class InvalidDistanceError(SolarSystemError): # если дистанция от Солнца <= 0
     pass
