e74c7aa8b07a251013536a341132078fcb5ed70d
suyeatolog <pk080407@gmail.com>
2026-02-17 12:34:42 +0300

    feat: Spacecraft class

---
+++ lab1/src/spacecraft.py
@@ -0,0 +1,76 @@
+from src.celestial_body import CelestialBody
+from src.validators.spacecraft_validator import SpacecraftValidator
+from exceptions.custom_exceptions import LaunchError
+
+class Spacecraft(CelestialBody):
+    def __init__(
+        self,
+        fuel: float = 1000.0,
+        status: str = "idle",
+    ):
+        super().__init__()
+        SpacecraftValidator.validate_fuel(fuel)
+        SpacecraftValidator.validate_status(status)
+
+        self._fuel = fuel
+        self._status = status
+        self._destination = None
+
+    @property
+    def fuel(self) -> float:
+        return self._fuel
+
+    @fuel.setter
+    def fuel(self, value: float):
+        SpacecraftValidator.validate_fuel(value)
+        self._fuel = value
+
+    @property
+    def status(self) -> str:
+        return self._status
+
+    @status.setter
+    def status(self, value: str):
+        SpacecraftValidator.validate_status(value)
+        self._status = value
+
+    @property
+    def destination(self) -> CelestialBody:
+        return self._destination
+
+    @destination.setter
+    def destination(self, value: CelestialBody):
+        self._destination = value
+
+    def launch(self):
+        if self.fuel <= 0:
+            raise LaunchError("Недостаточно топлива для запуска")
+        self.status = "traveling"
+
+    def travel_to(self, target: CelestialBody):
+        if self.fuel <= 0:
+            raise LaunchError("Недостаточно топлива для путешествия")
+        self.destination = target
+        self.status = "traveling"
+
+        self.position = target.position
+        self.status = "idle"
+
+    def collect_data(self, target: CelestialBody) -> dict:
+
+        return {
+            "object_name": target.name,
+            "mass": target.mass,
+            "position": target.position,
+            "radius": target.radius
+        }
+
+    def move(self, dt: float):
+
+        vx: float = 0.1
+        x, y, z = self.position
+        self.position = (x + vx * dt, y, z)
+
+    def get_info(self) -> str:
+        base_info = super().get_info()
+        return f"{base_info}, статус={self.status}, топливо={self.fuel}, цель={self.destination.name if self.destination else 'нет'}"
